56a089d4ae59be1c16d4982745b7dcaf8b5665ef
emp-norton <jamesjelenko@gmail.com>
2018-04-10 18:43:22 -0700

    add endpoint for api call

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const key = require('../config.js') //"MW9S-E7SL-26DU-VV8V"; // public key, doesn't need to be env var
-const $ = require('jquery');
 const app = express();
+const request = require('https');
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
@@ -16,19 +16,19 @@ app.get('/', (req, res) => {
 	res.send('ok');
 });
 
-app.get('/api', (req, res) => {
-	 let url = `https://api.bart.gov/api/etd.aspx?cmd=etd&orig=${origin}&key=MW9S-E7SL-26DU-VV8V&json=y`;
-		$.ajax({
-      type: 'GET',
-      url: '/api',
-      success: function(data) {
-      	console.log(data)
-        res.send(data)
-      }, 
-      failure: function(err) {
-        console.log(err)
-      }
-   })
+app.get('/api', (req, response) => {
+	 let url = `https://api.bart.gov/api/etd.aspx?cmd=etd&orig=powl&key=MW9S-E7SL-26DU-VV8V&json=y`;
+	 request.get(url, res => {
+	 	let body = '';
+	 	res.on('data', data => {
+	 		body += data;;
+	 	})
+	 	res.on('end', () => {
+	 		body = JSON.parse(body);
+	 		console.log(body)
+	 		response.send(body);
+	 	})
+	 })
 })
 
 
